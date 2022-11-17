8a6a001308a855e8cf275272c73db088de7d45d1
AmLibra <tenchishishou@hotmail.com>
2022-11-17 20:55:33 +0100

    Add ListView of cards returned by the API.

--- src/main/java/com/libra/ptcgt/ptcgtool/Controller.java
+++ src/main/java/com/libra/ptcgt/ptcgtool/Controller.java
@@ -2,23 +2,30 @@ package com.libra.ptcgt.ptcgtool;
 
 import com.libra.ptcgt.ptcgtool.api.PTCGAPI;
 import com.libra.ptcgt.ptcgtool.objects.Card;
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
     @FXML
-    private Label text;
+    private ListView<Card> lView;
     @FXML
     private TextField searchField;
 
     @FXML
     protected void search() {
+        List<Card> cards = new ArrayList<>();
         String s = searchField.textProperty().get();
-        StringBuilder acc = new StringBuilder();
         searchField.textProperty().setValue("");
-        PTCGAPI.searchCardData(s).forEach(v -> acc.append(new Card(v)).append("\n"));
-        text.setText(acc.toString());
+        PTCGAPI.searchCardData(s).forEach(v -> cards.add(new Card(v)));
+        lView.getItems().addAll(cards);
     }
 }
